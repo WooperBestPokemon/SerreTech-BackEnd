282bcde8409ef8ebbd9574d2a6c4a8b90b5d2499
Jacob Maltais <felixnya@protonmail.com>
2021-09-03 15:57:27 -0700

    Creating a script that controls the pumps using a 5v relay

---
+++ scripts/pump-script.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+argument = sys.argv[1]
+
+
+#Calculating the number of seconds
+
+seconds = float(argument) / 30
+
+#Starting the pump and letting out the water
+
+GPIO.setmode(GPIO.BCM)
+
+RELAIS_1_GPIO = 20
+GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)
+GPIO.output(RELAIS_1_GPIO, GPIO.LOW)
+print("[+] The pump will be running for {} seconds...".format(seconds))
+time.sleep(seconds)
+GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)
+print("[-] the pump has been shutdown.")
