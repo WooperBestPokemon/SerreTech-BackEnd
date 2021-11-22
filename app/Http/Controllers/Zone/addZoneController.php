862c8affe8ba60aaf775763d6bae68cfe3a5cb49
Mathieu Bienvenu <1930528@etu.cegepjonquiere.ca>
2021-11-22 08:29:30 -0500

    Mise a jour API

--- app/Http/Controllers/Zone/addZoneController.php
+++ app/Http/Controllers/Zone/addZoneController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Zone;
 
 use App\Http\Controllers\Controller;
+use App\Models\GreenHouse;
 use App\Models\Zone;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class addZoneController extends Controller
@@ -16,7 +18,6 @@ class addZoneController extends Controller
     public function insert(Request $request){
         $request->validate([
             'name'=>'required|string',
-            'description'=>'required|string',
             'typeFood'=>'required|string'
         ]);
 
@@ -34,11 +35,20 @@ class addZoneController extends Controller
     }
 
     public function __invoke(){
-
+        $user = Auth::user();
+        $greenhouses =[];
+        foreach (GreenHouse::where('idCompany','=',$user->idCompany)->get() as $greenhouse) {
+            array_push($greenhouses, [
+                "idGreenHouse" => $greenhouse->getAttributes()["idGreenHouse"],
+                "name" => $greenhouse->getAttributes()["name"],
+                "description" => $greenhouse->getAttributes()["description"],
+                "img" => $greenhouse->getAttributes()["img"],
+            ]);
+        }
         $url = 'http://apipcst.xyz/api/searchAll/plant ';
         $response = file_get_contents($url);
         $newsData = json_decode($response);
-        return view('addZone',['allPlant' => $newsData]);
+        return view('addZone',['allPlant' => $newsData,'greenhouse' => $greenhouses]);
     }
 
 
