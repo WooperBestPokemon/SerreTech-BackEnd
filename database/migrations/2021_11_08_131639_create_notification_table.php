8d7dd986e65fa0ef4e5ae8943120add8e5fe4d51
Mathieu Bienvenu <1930528@etu.cegepjonquiere.ca>
2021-12-02 16:55:14 -0500

    Bug Fix Merge

---
+++ database/migrations/2021_11_08_131639_create_notification_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblnotification', function (Blueprint $table) {
+            $table->id('idAlerte')->autoIncrement();
+            $table->string('description');
+            $table->boolean('alerteStatus');           
+            $table->timestamps();
+            $table->foreignId('idSensor');
+            $table->foreign('idSensor')->references('idSensor')->on('tblSensor')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tblNotification');
+    }
+}
